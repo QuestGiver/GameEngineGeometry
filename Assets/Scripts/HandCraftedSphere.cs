6138f80a80a7c59e78a5cc96dafe1fb8f90f0a28
QuestGiver <cpt.gastard@gmail.com>
2018-08-16 16:29:02 -0700

    made small edits

--- Assets/Scripts/HandCraftedSphere.cs
+++ Assets/Scripts/HandCraftedSphere.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -128,5 +129,13 @@ public class HandCraftedSphere : MonoBehaviour
     void Update()
     {
 
+    }
+
+
+    Vector3[] GetVerts()//old
+    {
+
+
+        return Verts;
     }
 }
