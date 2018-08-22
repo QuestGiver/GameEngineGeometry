fa81e7e947a1ea22dc3c4f2d6194938999ef63c6
QuestGiver <cpt.gastard@gmail.com>
2018-08-21 17:54:53 -0700

    add unity chans fireball

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 10;
+    public int isunning = 1;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 normalizedMoevment = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isunning = 2;
+        }
+        else
+        {
+            isunning = 1;
+        }
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            normalizedMoevment += transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            normalizedMoevment += -transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            normalizedMoevment += -transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            normalizedMoevment += transform.right;
+        }
+
+
+
+        controller.Move(normalizedMoevment.normalized * speed * isunning * Time.deltaTime);
+        controller.Move(new Vector3(0, -9.8f, 0) * Time.deltaTime);
+
+
+    }
+
+}
---
+++ Assets/Scripts/VCamSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VCamSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] gameObjects;
+
+    public GameObject lastActive;
+
+
+    public void SwitchTo(int i)
+    {
+        if (lastActive != null)
+        {
+            lastActive.SetActive(false);
+        }
+
+        gameObjects[i].SetActive(true);
+        lastActive = gameObjects[i];
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        SwitchTo(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.Q))) { SwitchTo(0); }
+        if ((Input.GetKey(KeyCode.Q))) { SwitchTo(1); }
+        if ((Input.GetKey(KeyCode.Q))) { SwitchTo(2); }
+        if ((Input.GetKey(KeyCode.Q))) { SwitchTo(3); }
+
+
+    }
+}
