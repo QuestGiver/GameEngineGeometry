0d87c6edbe405ecd7090ad08a2526c006e70b3a3
QuestGiver <cpt.gastard@gmail.com>
2018-09-05 12:50:48 -0700

    add perlin texture generator

--- Assets/Scripts/AIEProject/ParticleFloor.cs
+++ Assets/Scripts/AIEProject/ParticleFloor.cs
@@ -4,31 +4,66 @@ using UnityEngine;
 
 public class ParticleFloor : MonoBehaviour
 {
-
+    public int sizeX;
+    public int sizeY;
 
 
     public ParticleSystem particleSystem;
     // Use this for initialization
     void Start()
     {
-        particleSystem = GetComponent<ParticleSystem>();
+        //particleSystem = new ParticleSystem;
+
+        ParticleSystem.MainModule main = particleSystem.main;
+        main.startSpeed = 0;
+        main.startLifetime = Mathf.Infinity;
+        main.loop = false;
+        main.maxParticles = sizeX * sizeY;
+        
+
+        ParticleSystem.ShapeModule shape = particleSystem.shape;
+        shape.shapeType = ParticleSystemShapeType.Circle;
+        shape.radius = 0;
+        shape.radiusThickness = 0;
+
+        ParticleSystem.EmissionModule emission = particleSystem.emission;
+        emission.rateOverTime = 0;
+
+
+
+
+
+        for (int x = 0; x < sizeX; x++)
+        {
+            particleSystem.Emit(1);
+            for (int y = 0; y < sizeY; y++)
+            {
+                
+            }
+
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
         
     }
 
-    public void ParticleUpdate(ParticleSystem system)
-    {
-        //ParticleSystem.EmitParams emitParams;
-        //emitParams.position
-        ParticleSystem.EmissionModule emission = particleSystem.emission;
-        //Mathf.PerlinNoise
-        //emission.rateOverTime = 
-    }
+    //public ParticleSystem ParticleSetUp(ParticleSystem system)
+    //{
+    //    ParticleSystem.MainModule main = system.main;
+        
+    //    main.startSpeed = 0;
+    //    main.startLifetime = Mathf.Infinity;
+    //    main.loop = false;
+
+        
+        
+        
+
+    //    return 
+    //}
 
 }
--- Assets/Scripts/AIEProject/PerlinTexture.cs
+++ Assets/Scripts/AIEProject/PerlinTexture.cs
@@ -4,16 +4,70 @@ using UnityEngine;
 
 public class PerlinTexture : MonoBehaviour
 {
+    //scrolls the texture
+    public float scrollspeed = 1;
+
+    // Width and height of the texture in pixels.
+    public int pixWidth;
+    public int pixHeight;
+
+    // The origin of the sampled area in the plane.
+    public float xOrg;
+    public float yOrg;
+
+    // The number of cycles of the basic noise pattern that are repeated
+    // over the width and height of the texture.
+    public float scale = 1.0F;
+
+    public Texture2D noiseTex;
+    private Color[] pix;
+    private Renderer rend;
+
+
 
     // Use this for initialization
     void Start()
     {
+        rend = GetComponent<Renderer>();
 
+        // Set up the texture and a Color array to hold pixels during processing.
+        noiseTex = new Texture2D(pixWidth, pixHeight);
+        pix = new Color[noiseTex.width * noiseTex.height];
+        rend.material.mainTexture = noiseTex;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        xOrg += Time.deltaTime * scrollspeed;
+        yOrg += Time.deltaTime * scrollspeed;
+        CalcNoise();
     }
+
+    void CalcNoise()
+    {
+        // For each pixel in the texture...
+        float y = 0.0F;
+
+        while (y < noiseTex.height)
+        {
+            float x = 0.0F;
+            while (x < noiseTex.width)
+            {
+                float xCoord = xOrg + x / noiseTex.width * scale;
+                float yCoord = yOrg + y / noiseTex.height * scale;
+                float sample = Mathf.PerlinNoise(xCoord, yCoord);
+                pix[(int)y * noiseTex.width + (int)x] = new Color(sample, sample, sample);
+                x++;
+            }
+            y++;
+        }
+
+        // Copy the pixel data to the texture and load it into the GPU.
+        noiseTex.SetPixels(pix);
+        noiseTex.Apply();
+    }
+
+
 }
