7b44e89ad3ca2cad1a4493d9855777f347aa22bd
QuestGiver <cpt.gastard@gmail.com>
2018-09-07 18:02:53 -0700

    add perlin floor

--- Assets/Scripts/AIEProject/ParticleFloor.cs
+++ Assets/Scripts/AIEProject/ParticleFloor.cs
@@ -5,25 +5,29 @@ using UnityEngine;
 [RequireComponent(typeof(ParticleSystem))]
 public class ParticleFloor : MonoBehaviour
 {
+
+    public float xOrg = 0;
+    public float yOrg = 0;
+    public float scrollSpeed = 1;
+    public float noiseScale = 1;
+    public float amplitudeScale = 1;
+
     public int sizeX;
     public int sizeY;
     int numParticlesAlive;
-    public PerlinTexture perlinTexture;
     public new ParticleSystem particleSystem;
     ParticleSystem.Particle[] particles;
     public new ParticleSystemRenderer renderer;
+
+    Vector3[] rgb;
     // Use this for initialization
 
     void Start()
     {
         renderer = GetComponent<ParticleSystemRenderer>();
-        perlinTexture = GetComponent<PerlinTexture>();
+        //perlinTexture = GetComponent<PerlinTexture>();
         particleSystem = GetComponent<ParticleSystem>();
 
-        if (particles == null || particles.Length < particleSystem.main.maxParticles)
-        {
-            particles = new ParticleSystem.Particle[particleSystem.main.maxParticles];
-        }
 
         var main = particleSystem.main;
         main.startSpeed = 0;
@@ -39,24 +43,39 @@ public class ParticleFloor : MonoBehaviour
 
         var emission = particleSystem.emission;
         emission.rateOverTime = 0;
+        emission.enabled = false;
 
         //renderer = GetComponent<ParticleSystemRenderer>();
         renderer.renderMode = ParticleSystemRenderMode.Mesh;
-        renderer.mesh = Resources.GetBuiltinResource<Mesh>("Capsule.fbx");
+        renderer.mesh = Resources.GetBuiltinResource<Mesh>("Cube.fbx");
         renderer.alignment = ParticleSystemRenderSpace.World;
-      
+        renderer.enabled = true;
 
 
 
+        ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
+
+
+        //sizeX = sizeY = 10;
         for (int x = 0; x < sizeX; x++)
         {
             for (int y = 0; y < sizeY; y++)
             {
-                transform.Translate(new Vector3(0, 0, 1));
-                particleSystem.Emit(1);
+                emitParams.position = new Vector3(shape.position.x + x, shape.position.y, shape.position.z + y);
+                particleSystem.Emit(emitParams, 1);
             }
-            transform.Translate(new Vector3(1, 0, -sizeY));
         }
+
+
+        if (particles == null || particles.Length < particleSystem.main.maxParticles)
+        {
+            Debug.Log(" count = " + particleSystem.particleCount);
+            particles = new ParticleSystem.Particle[particleSystem.particleCount];
+            particleSystem.GetParticles(particles);
+        }
+
+
+        rgb = new Vector3[sizeX * sizeY];
 
 
 
@@ -65,29 +84,61 @@ public class ParticleFloor : MonoBehaviour
 
 
     // Update is called once per frame
-    
+
     void Update()
     {
-        // Change only the particles that are alive
-        int xTotal = 0;
-        int yTotal = 0;
-        for (int x = 0; x < sizeX; x++)
+        xOrg += Time.deltaTime * scrollSpeed;
+        yOrg += Time.deltaTime * scrollSpeed;
+
+
+        //int width = (int) Mathf.Sqrt(particles.Length);
+
+
+
+
+        for (int idx = 0; idx < particles.Length; idx++)
         {
-            xTotal++;
-            for (int y = 0; y < sizeY; y++)
-            {
-                yTotal++;
-                particles[xTotal + yTotal].position = new Vector3(0, perlinTexture.noiseTex.GetPixel(x,y).grayscale, 0);
-                if (perlinTexture.noiseTex == null)
-                {
-                    Debug.Log("Noise texture is null");
-                }
-            }
+            //y = idx / width;
+            //x = idx - y * width;
+            //x = (x / width) * tw;
+            //y = (y / width) * th;
+            //PerlinTexture.noiseTex.GetPixel(idx / width, idx - ((idx / width) * width)).grayscale       [(idx / width, idx - ((idx / width) * width))]
+
+            float greyScale = PerlinTexture.VectorToGreyScale(PerlinTexture.ColorVector(sizeX, sizeY, rgb, xOrg, yOrg, noiseScale, amplitudeScale)[idx]);
+            particles[idx].position = new Vector3(particles[idx].position.x, greyScale, particles[idx].position.z);
+
         }
 
-
-
-        // Apply the particle changes to the particle system
+        //StartCoroutine(Grey());
         particleSystem.SetParticles(particles, numParticlesAlive);
+    }
+
+
+    IEnumerator Grey()
+    {
+        while (true)
+        {
+            xOrg += Time.deltaTime * scrollSpeed;
+            yOrg += Time.deltaTime * scrollSpeed;
+
+
+            //int width = (int) Mathf.Sqrt(particles.Length);
+
+
+
+
+            for (int idx = 0; idx < particles.Length; idx++)
+            {
+                //y = idx / width;
+                //x = idx - y * width;
+                //x = (x / width) * tw;
+                //y = (y / width) * th;
+                //PerlinTexture.noiseTex.GetPixel(idx / width, idx - ((idx / width) * width)).grayscale       [(idx / width, idx - ((idx / width) * width))]
+
+                float greyScale = PerlinTexture.VectorToGreyScale(PerlinTexture.ColorVector(sizeX, sizeY, rgb, xOrg, yOrg, noiseScale, amplitudeScale)[idx]);
+                particles[idx].position = new Vector3(particles[idx].position.x, greyScale, particles[idx].position.z);
+                yield return null;
+            }
+        }
     }
 }
--- Assets/Scripts/AIEProject/PerlinTexture.cs
+++ Assets/Scripts/AIEProject/PerlinTexture.cs
@@ -2,52 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PerlinTexture : MonoBehaviour
+public static class PerlinTexture
 {
-    //scrolls the texture
-    public float scrollspeed = 1;
 
-    // Width and height of the texture in pixels.
-    public int pixWidth;
-    public int pixHeight;
-
-    // The origin of the sampled area in the plane.
-    public float xOrg;
-    public float yOrg;
-
-    // The number of cycles of the basic noise pattern that are repeated
-    // over the width and height of the texture.
-    public float scale = 1.0F;
-
-    public Texture2D noiseTex;
-    private Color[] pix;
-    private Renderer rend;
-
-
-
-    // Use this for initialization
-    void Start()
-    {
-        rend = GetComponent<Renderer>();
-
-        // Set up the texture and a Color array to hold pixels during processing.
-        noiseTex = new Texture2D(pixWidth, pixHeight);
-        pix = new Color[noiseTex.width * noiseTex.height];
-        rend.material.mainTexture = noiseTex;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        xOrg += Time.deltaTime * scrollspeed;
-        yOrg += Time.deltaTime * scrollspeed;
-        CalcNoise();
-    }
-
-    void CalcNoise()
+    public static Texture2D CalcNoise(Texture2D noiseTex, int pixHeight, int pixWidth, float xOrg, float yOrg, float scale, float amplitude)
     {
         // For each pixel in the texture...
+        Color[] pix;
+        pix = new Color[noiseTex.width * noiseTex.height];
         float y = 0.0F;
 
         while (y < noiseTex.height)
@@ -57,7 +19,7 @@ public class PerlinTexture : MonoBehaviour
             {
                 float xCoord = xOrg + x / noiseTex.width * scale;
                 float yCoord = yOrg + y / noiseTex.height * scale;
-                float sample = Mathf.PerlinNoise(xCoord, yCoord);
+                float sample = Mathf.PerlinNoise(xCoord, yCoord) * amplitude;
                 pix[(int)y * noiseTex.width + (int)x] = new Color(sample, sample, sample);
                 x++;
             }
@@ -67,7 +29,67 @@ public class PerlinTexture : MonoBehaviour
         // Copy the pixel data to the texture and load it into the GPU.
         noiseTex.SetPixels(pix);
         noiseTex.Apply();
+
+        return noiseTex;
     }
 
+
+    public static IEnumerable<Vector3[]> CoColorVector(int pixHeight, int pixWidth, Vector3[] rgb, float xOrg, float yOrg, float scale, float amplitude)
+    {
+         
+        float y = 0.0F;
+
+        while (y < pixHeight)
+        {
+            float x = 0.0F;
+            while (x < pixWidth)
+            {
+                float xCoord = xOrg + x / pixWidth * scale;
+                float yCoord = yOrg + y / pixHeight * scale;
+                float sample = Mathf.PerlinNoise(xCoord, yCoord) * amplitude;
+                rgb[(int)y * pixWidth + (int)x] = new Vector3(sample, sample, sample);
+                x++;
+            }
+            y++;
+            yield return rgb;
+        }
+
+    }
+
+    public static Vector3[] ColorVector(int pixHeight, int pixWidth, Vector3[] rgb, float xOrg, float yOrg, float scale, float amplitude)
+    {
+
+        float y = 0.0F;
+
+        while (y < pixHeight)
+        {
+            float x = 0.0F;
+            while (x < pixWidth)
+            {
+                float xCoord = xOrg + x / pixWidth * scale;
+                float yCoord = yOrg + y / pixHeight * scale;
+                float sample = Mathf.PerlinNoise(xCoord, yCoord) * amplitude;
+                rgb[(int)y * pixWidth + (int)x] = new Vector3(sample, sample, sample);
+                x++;
+            }
+            y++;
+            
+        }
+        return rgb;
+    }
+
+    public static float VectorToGreyScale(Vector3 rgbVect)
+    {
+
+        return (rgbVect.x + rgbVect.y + rgbVect.z) / 3;
+
+    }
+
+    public static float VectorToGreyScale(IEnumerator<Vector3> rgbVect)
+    {
+
+        return (rgbVect.Current.x + rgbVect.Current.y + rgbVect.Current.z) / 3;
+
+    }
 
 }
